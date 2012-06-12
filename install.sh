12f1e3e74e911c810782871ebf57cfa93c6268fc
Kenta Suzuki <suzuken326@gmail.com>
2012-06-12 23:59:18 +0900

    modify install script

--- install.sh
+++ install.sh
@@ -2,23 +2,23 @@
 cd $(dirname $0)
 
 # install vundle.git
-if [ ! -f ~/.vim/bundle/vundle ]; then
-    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+if [ ! -f "$PWD/.vim/bundle/vundle" ]; then
+    git clone http://github.com/gmarik/vundle.git "$PWD/.vim/bundle/vundle"
 fi
 
 # update submodule
 git submodule init
 git submodule update
-if [ -f oh-my-zsh ]; then
-    cd oh-my-zsh
+if [ -f "$PWD/oh-my-zsh" ]; then
+    cd .oh-my-zsh
     git checkout master
+    cd ..
 fi
 
 # create symbolic link
 for dotfile in .?*
 do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.gitmodules']
-    then
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.gitmodules' ]; then
         ln -Fis "$PWD/$dotfile" $HOME
     fi
 done
