6fa4c79c7db8fa2dd9a893c5ce80082bfcb0134d
Kenta Suzuki <suzuken326@gmail.com>
2012-06-12 21:24:02 +0900

    modify scripts for installation

--- install.sh
+++ install.sh
@@ -2,7 +2,17 @@
 cd $(dirname $0)
 
 # install vundle.git
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+if [ ! -f ~/.vim/bundle/vundle ]; then
+    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
+
+# update submodule
+git submodule init
+git submodule update
+if [ -f oh-my-zsh ]; then
+    cd oh-my-zsh
+    git checkout master
+fi
 
 # create symbolic link
 for dotfile in .?*
